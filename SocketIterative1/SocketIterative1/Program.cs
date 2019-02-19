8a3f43d67da331148eeb677013add232ab18cfdf
GitteSB <36244161+GitteSB@users.noreply.github.com>
2019-02-19 21:08:22 +0100

    Add files via upload

---
+++ SocketIterative1/SocketIterative1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketIterative1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // IPAddress ip = new IPAddress("127.0.0.1");
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+
+            //Alternatively but deprecated
+            //TcpListener serverSocket = new TcpListener(6789);
+
+            serverSocket.Start();
+            Console.WriteLine("Server started");
+
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                TCPEchoServer echoServer = new TCPEchoServer(connectionSocket);
+                echoServer.DoIt();
+            }
+            
+        }
+    }
+}
---
+++ SocketIterative1/SocketIterative1/TCPEchoServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketIterative1
+{
+    public class TCPEchoServer
+    {
+        private TcpClient _connectionSocket;
+
+        public TCPEchoServer(TcpClient client)
+        {
+            _connectionSocket = client;
+                                           
+        }
+        public void DoIt()
+        {
+            NetworkStream ns = _connectionSocket.GetStream();
+
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+
+            sw.AutoFlush = true;
+
+            string message = sr.ReadLine();
+
+            while (message != "stop")
+            {
+                Console.WriteLine(message);
+                sw.WriteLine(message.ToUpper());
+                message = sr.ReadLine();
+            }
+            ns.Close();
+            _connectionSocket.Close();
+
+
+
+
+        }
+    }
+}
